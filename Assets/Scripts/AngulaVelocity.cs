f121f58a2d3d10efd4d632e28836d819385ab901
mynameisFer <cocoferrika2549@gmail.com>
2026-03-11 19:21:44 +0700

    Start AngulaVelocity Script

--- Assets/Scripts/AngulaVelocity.cs
+++ Assets/Scripts/AngulaVelocity.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class AngulaVelocity : MonoBehaviour
 {
+
+    Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
