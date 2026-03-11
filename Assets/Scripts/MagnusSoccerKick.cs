9469de50ca28c8fd446259a5492c141bab839ce3
mynameisFer <cocoferrika2549@gmail.com>
2026-03-11 20:38:56 +0700

    kick script

--- Assets/Scripts/MagnusSoccerKick.cs
+++ Assets/Scripts/MagnusSoccerKick.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MagnusSoccerKick : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float kickForce;
+    public float spinAmount;
+    public float magnusStrength = 0.5f;
+
+    Rigidbody rb;
+    bool isShot = false;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && !isShot)
+        {
+            rb.AddForce(Vector3.left * kickForce, ForceMode.Impulse);
+
+            rb.AddTorque(Vector3.up * spinAmount);
+            isShot = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        if (!isShot) return;
+        Vector3 velocity = rb.linearVelocity;
+        Vector3 spin = rb.angularVelocity;
+        Vector3 magnusforce = magnusStrength * Vector3.Cross(spin, velocity);
+
+        rb.AddForce(magnusforce);
     }
 }
